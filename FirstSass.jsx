7961cd445bdd0ca300e5a48b25fabcdbffba6491
Arup Kumar Behera <76170236+ArupGhoost@users.noreply.github.com>
2021-04-10 18:29:17 +0530

    Add files via upload

---
+++ FirstSass.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './FirstSass.scss'
+
+
+const FirstSass = () => {
+    return (
+        <>
+        <div className='main'>
+         <h1>First Time SassProjects</h1>
+         
+         <br />
+         <h3>New Sass Projects Coming Soon</h3>
+         </div>   
+        </>
+    )
+}
+
+export default FirstSass
